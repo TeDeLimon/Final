7c5b5735bd3926c14f0919a40c981dbdf977f783
TeDeLimon <106811528+TeDeLimon@users.noreply.github.com>
2022-10-14 11:41:37 +0200

    12
    123

--- includes/config/database.php
+++ includes/config/database.php
@@ -8,6 +8,7 @@ function conectarDb(): mysqli
         $_ENV['DB_PASS'], 
         $_ENV['DB_BD']
     );
+    $mysqli->set_charset("utf8mb4");
 
     if (!$db) {
         echo "Error: No se pudo conectar a MySQL.";
